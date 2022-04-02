c80ea62a4dca1a39e203d15386e69cd9bc38ebde
Virginia <virginia.carmuega@gmail.com>
2022-04-02 11:09:38 -0300

    4º project finished

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return <main>
+    <div className="container">
+      <h3>questions and answers about loggin</h3>
+      <section className="info">
+      {data.map((Q)=>{return <SingleQuestion key={Q.id} {...Q} />})}
+      </section>
+    </div>
+  </main>;
 }
 
 export default App;
